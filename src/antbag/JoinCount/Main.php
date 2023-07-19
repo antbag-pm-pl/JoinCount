71f28920b71ff8b2bd8d6afd5b6c31205975bf9e
notant <84870445+antbag-dev@users.noreply.github.com>
2023-07-19 19:17:47 +0500

    Create config

--- src/antbag/JoinCount/Main.php
+++ src/antbag/JoinCount/Main.php
@@ -9,6 +9,8 @@ class Main extends PluginBase {
 
     public function onEnable(): void {
         // Register event listener
+        $config->save();
+        $config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
         $listener = new PlayerJoinListener($this->getDataFolder() . "playerData.json");
         $this->getServer()->getPluginManager()->registerEvents($listener, $this);
     }
