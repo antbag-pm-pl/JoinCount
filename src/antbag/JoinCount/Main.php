9f0a7f756d431c096d1a307525999fe388cf278f
notant <84870445+antbag-dev@users.noreply.github.com>
2023-07-24 18:30:04 +0500

    Oops

--- src/antbag/JoinCount/Main.php
+++ src/antbag/JoinCount/Main.php
@@ -4,14 +4,14 @@ namespace antbag\JoinCount;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\util\Config;
+use pocketmine\utils\Config;
 
 class Main extends PluginBase {
 
     public function onEnable(): void {
         // Register event listener
         $config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-        $config->save();
+        $this->saveResource("config.yml");
         $listener = new PlayerJoinListener($this->getDataFolder() . "playerData.json");
         $this->getServer()->getPluginManager()->registerEvents($listener, $this);
     }
