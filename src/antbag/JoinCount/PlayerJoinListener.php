a6279cf074f80566a8f37df0f7258171d60d02a4
notant <84870445+antbag-dev@users.noreply.github.com>
2023-07-25 22:21:39 +0500

    Update PlayerJoinListener.php

--- src/antbag/JoinCount/PlayerJoinListener.php
+++ src/antbag/JoinCount/PlayerJoinListener.php
@@ -29,7 +29,7 @@ class PlayerJoinListener implements Listener {
             $totalPlayers = count($data);
 
             $welcomeMessage = Main::getInstance()->config->getNested("WelcomeMessage");
-            $welcomeMessage = str_replace("{player}", $welcomeMessage);
+            $welcomeMessage = str_replace("{player}", $playerName, $welcomeMessage);
             $welcomeMessage = str_replace("{total_players}", $totalPlayers, $welcomeMessage);
             Server::getInstance()->broadcastMessage($welcomeMessage);
         }
