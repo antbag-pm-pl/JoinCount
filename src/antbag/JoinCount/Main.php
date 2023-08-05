12cd5b88ce866d1c32d5d4172932e8306e80db2f
notant <84870445+antbag-dev@users.noreply.github.com>
2023-08-05 23:04:59 +0500

    Update Main.php

--- src/antbag/JoinCount/Main.php
+++ src/antbag/JoinCount/Main.php
@@ -6,18 +6,15 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\utils\Config;
 use antbag\JoinCount\PlayerJoinListener;
-use CortexPE\DiscordWebhookAPI\Webhook;
+
 
 class Main extends PluginBase {
 
     public static $instance;
-    public Config $config;
+    
     
     public function onEnable(): void {
         // Register event listener
-        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-        $this->saveResource("config.yml");
-        $url = $this->config->getNested("DiscordURL");
         self::$instance = $this;
         $listener = new PlayerJoinListener($this->getDataFolder() . "playerData.json");
         $this->getServer()->getPluginManager()->registerEvents($listener, $this);
