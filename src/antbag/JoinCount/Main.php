94031c6c98dde18b7c5f9dee3b7f8c74a87b07c8
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-08-17 21:49:52 +0700

    Promotion of libBedrock library :)

--- src/antbag/JoinCount/Main.php
+++ src/antbag/JoinCount/Main.php
@@ -1,25 +1,31 @@
 <?php
 
+declare(strict_types=1);
+
 namespace antbag\JoinCount;
 
+use NhanAZ\libBedrock\Counter;
+use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\utils\Config;
-use antbag\JoinCount\PlayerJoinListener;
 
+class Main extends PluginBase implements Listener {
 
-class Main extends PluginBase {
+    private int $totalPlayers;
 
-    public static $instance;
-    
-    
-    public function onEnable(): void {
+    protected function onEnable(): void {
         // Register event listener
-        self::$instance = $this;
-        $listener = new PlayerJoinListener($this->getDataFolder() . "playerData.json");
-        $this->getServer()->getPluginManager()->registerEvents($listener, $this);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->totalPlayers = Counter::getFileInfo($this->getServer()->getDataPath() . "players", ".dat")["totalFiles"];
     }
-    public static function getInstance(): Main{
-        return self::$instance;
+
+    public function onPlayerJoin(PlayerJoinEvent $event): void {
+        $player = $event->getPlayer();
+        $playerName = $player->getName();
+        if (!$player->hasPlayedBefore()) {
+            $this->totalPlayers++;
+            $welcomeMessage = "§8Welcome,§c $playerName! §8You are the #§8 " . $this->totalPlayers . " §8player to join our server!";
+            $this->getServer()->broadcastMessage($welcomeMessage);
+        }
     }
 }
