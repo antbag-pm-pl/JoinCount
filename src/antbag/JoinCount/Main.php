a2ecf0a0154f94d8dc6d959f427cb952093e96e5
Your Name <abdulrhemanrashid@gmail.com>
2024-05-16 01:06:56 +0500

    fixxx

--- src/antbag/JoinCount/Main.php
+++ src/antbag/JoinCount/Main.php
@@ -3,23 +3,11 @@
 namespace antbag\JoinCount;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\utils\Config;
-use antbag\JoinCount\PlayerJoinListener;
-
 
 class Main extends PluginBase {
 
-    public static $instance;
-    
-    
-    public function onEnable(): void {
-        // Register event listener
-        self::$instance = $this;
-        $listener = new PlayerJoinListener($this->getDataFolder() . "playerData.json");
-        $this->getServer()->getPluginManager()->registerEvents($listener, $this);
-    }
-    public static function getInstance(): Main{
-        return self::$instance;
-    }
+  public function onEnable(): void {
+    $listener = new PlayerJoinListener($this->getDataFolder() . "playerData.json");
+    $this->getServer()->getPluginManager()->registerEvents($listener, $this);
+  }
 }
