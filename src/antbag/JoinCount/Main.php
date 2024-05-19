d8a71145139b6aabacbd2e06198238a069aa6eff
Lixツ <ktostamhip@gmail.com>
2024-05-19 10:26:24 +0200

    Update Main.php
    Added a configuration for the message

--- src/antbag/JoinCount/Main.php
+++ src/antbag/JoinCount/Main.php
@@ -1,5 +1,3 @@
-<?php
-
 namespace antbag\JoinCount;
 
 use pocketmine\plugin\PluginBase;
@@ -7,7 +5,9 @@ use pocketmine\plugin\PluginBase;
 class Main extends PluginBase {
 
   public function onEnable(): void {
-    $listener = new PlayerJoinListener($this->getDataFolder() . "playerData.json");
+    $this->saveDefaultConfig(); // Copy default config.yml if it doesn't exist
+    $config = $this->getConfig()->getAll();
+    $listener = new PlayerJoinListener($this->getDataFolder() . "playerData.json", $config);
     $this->getServer()->getPluginManager()->registerEvents($listener, $this);
   }
 }
