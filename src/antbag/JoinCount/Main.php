8cd78386eb8a8e3e2c0bd61cf0f9b2070661d46d
Lixツ <ktostamhip@gmail.com>
2024-05-19 21:00:41 +0200

    Update Main.php

--- src/antbag/JoinCount/Main.php
+++ src/antbag/JoinCount/Main.php
@@ -4,16 +4,38 @@ namespace antbag\JoinCount;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\permission\Permission;
 
 class Main extends PluginBase {
 
   private $config;
+  private $listener;
 
   public function onEnable(): void {
     $this->saveDefaultConfig();
     $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
     
-    $listener = new PlayerJoinListener($this->getDataFolder() . "playerData.json", $this->config);
-    $this->getServer()->getPluginManager()->registerEvents($listener, $this);
+    $this->listener = new PlayerJoinListener($this->getDataFolder() . "playerData.json", $this->config);
+    $this->getServer()->getPluginManager()->registerEvents($this->listener, $this);
+
+    // add permission
+    $perm = new Permission("joincount.totalplayers", "Allows the user to see the total number of players");
+    $this->getServer()->getPluginManager()->addPermission($perm);
+  }
+
+  public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+    if ($command->getName() === "totalplayers") {
+      if($sender->hasPermission("joincount.totalplayers")) {
+        $totalPlayers = count($this->listener->loadData());
+        $sender->sendMessage("Total players registered: " . $totalPlayers);
+        return true;
+      } else {
+        $sender->sendMessage("You do not have permission to use this command.");
+        return true;
+      }
+    }
+    return false;
   }
 }
