08a267686cb6b10e74a7b68b42e203b710f333aa
Lixツ <ktostamhip@gmail.com>
2024-05-19 10:40:15 +0200

    Update Main.php

--- src/antbag/JoinCount/Main.php
+++ src/antbag/JoinCount/Main.php
@@ -7,7 +7,7 @@ class Main extends PluginBase {
   public function onEnable(): void {
     $this->saveDefaultConfig(); // Copy default config.yml if it doesn't exist
     $config = $this->getConfig()->getAll();
-    $listener = new PlayerJoinListener($this->getDataFolder() . "playerData.json", $config);
+    $listener = new PlayerJoinListener($this->getDataFolder() . "playerData.json", $config, $this->getLogger());
     $this->getServer()->getPluginManager()->registerEvents($listener, $this);
     $this->getLogger()->info("Plugin enabled successfully.");
   }
